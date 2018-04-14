586b3b6140324fc1dce637aad6e2ce72d2e7a939
Daniel Foster <dfoster@sdsu.edu>
2018-04-14 14:50:26 -0700

    Preliminary design.

---
+++ YosemiteFishingRental/YosemiteFishingRental/Services/IEmailSender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace YosemiteFishingRental.Services
+{
+    public interface IEmailSender
+    {
+        Task SendEmailAsync(string email, string subject, string message);
+    }
+}
